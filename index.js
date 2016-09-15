13f2576322f81eb4317bc86e65605f915c71a328
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-09-16 00:34:13 +0530

    added the index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var config = {
+    channels: ["#kmitfoss", "#codecult"],
+    server: "irc.freenode.net",
+    botName: "iambot"
+};
+
+// Get the lib
+var irc = require("irc");
+
+// Create the bot name
+var bot = new irc.Client(config.server, config.botName, {
+    channels: config.channels
+});
+
+// Listen for joins
+bot.addListener("join", function(channel, who) {
+    // Welcome them in!
+    bot.say(channel, who + "...dude...welcome back!");
+});
